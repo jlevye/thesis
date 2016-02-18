1efc4d2a42009cd5d87accafba475b7662195c55
jlevye <jlevye@gmail.com>
2016-02-18 14:49:03 -0600

    Misc new files, plus .gitignore

---
+++ script_testing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+#Testing code, for debugging graph generating functions
+
+from graph_tool.all import *
+import numpy as np
+import math
+import os
+from PIL import Image
+
+
+os.chdir("/home/jen/Documents/School/GradSchool/Thesis/Code/")
+import scripts
+filename = "wobblytest.png" #Change for whatever the file is
+
+im = Image.open(filename).convert("L")
+image = np.array(im)
+scripts.invert(image)
+points = [[26,176],[51,157],[107,114],[32,54],[101,7],[120,70],[150,38],[192,91]]
+g, circleList = scripts.initGraph(points, image)
+g.save("debuggraph.xml.gz")
+graph_draw(g, pos = g.vp.coord, vertex_text = g.vertex_index)
